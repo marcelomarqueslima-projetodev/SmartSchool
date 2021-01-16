bf5bd61b55d1fa3455745b0b99d891dc0eaa9b31
Marcelo Marques de Lima <72136257+marcelomarqueslima-projetodev@users.noreply.github.com>
2021-01-16 07:35:14 -0300

    Criando repositorio

---
+++ SmartSchool.API/Data/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SmartSchool.API.Data
+{
+    public interface IRepository
+    {
+        void Add<T>(T entity) where T : class;
+        void Update<T>(T entity) where T : class;
+        void Delete<T>(T entity) where T : class;
+        bool SaveChanges();
+    }
+}
